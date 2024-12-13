3de8894b63ff28b8b7cf2a616cac4d4b3243512e
NTA Ho Huu Thang <thanghh.nta@gmail.com>
2024-12-13 14:15:01 +0700

    add

---
+++ chatgpt_code_review/download.py
@@ -0,0 +1,22 @@
+import display
+import streamlit as st
+
+
+def download_markdown(recommendations):
+    if recommendations:
+        st.download_button(
+            # "Download Markdown",
+            "Tải Markdown",
+            data=display.generate_markdown(recommendations),
+            file_name="chatgpt_recommendations.md",
+            mime="text/markdown",
+        )
+    else:
+        st.download_button(
+            # "Download Markdown",
+            "Tải Markdown",
+            data="",
+            file_name="chatgpt_recommendations.md",
+            mime="text/markdown",
+            disabled=True,
+        )
